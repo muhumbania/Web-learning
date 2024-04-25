fd58b91619d755d890900f7074605a76cb6a136b
muhumbania <franklinmuhumbania@gmail.com>
2024-04-25 21:52:09 +0200

    DB connection and items loaded from the DB

---
+++ Permalist+Project/8.6 Permalist Project/index.js
@@ -0,0 +1,50 @@
+import express from "express";
+import bodyParser from "body-parser";
+import pg from "pg";
+
+const app = express();
+const port = 3000;
+
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "permalist",
+  password: "postgresmhb",
+  port: 5432,
+});
+db.connect();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+let items = [];
+// let items = [
+//   { id: 1, title: "Buy milk" },
+//   { id: 2, title: "Finish homework" },
+// ];
+
+app.get("/", (req, res) => {
+  db.query('SELECT * FROM items', function(err, result){
+    items = result.rows;
+    console.log(result.rows);
+    res.render("index.ejs", {
+      listTitle: "Today",
+      listItems: items,
+    });
+  })
+  
+});
+
+// app.post("/add", (req, res) => {
+//   const item = req.body.newItem;
+//   items.push({ title: item });
+//   res.redirect("/");
+// });
+
+app.post("/edit", (req, res) => {});
+
+app.post("/delete", (req, res) => {});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
